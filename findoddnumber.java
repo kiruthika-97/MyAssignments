d81631c0623dce3287641c55c865981ac07926bf
kiruthika-97 <kiruthika9731@gmail.com>
2026-04-26 13:37:30 +0530

    Add files via upload
    day2

---
+++ findoddnumber.java
@@ -0,0 +1,14 @@
+public class findoddnumber {
+     public static void main(String[] args) {
+        
+       int maxRange = 10;
+
+        for (int i = 1; i <= maxRange; i++) {
+            
+        if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
